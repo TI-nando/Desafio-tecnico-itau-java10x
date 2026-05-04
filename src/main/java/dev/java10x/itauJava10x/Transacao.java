cbeececebd4f8faa221cd7808370cd3c03b19d78
TI-nando <fernandoagro.654@gmail.com>
2026-05-04 17:44:57 -0300

    feat: Criado EstatisticaDTO, e concluido o metodo Post 201, funcionando no Postman

--- src/main/java/dev/java10x/itauJava10x/Transacao.java
+++ src/main/java/dev/java10x/itauJava10x/Transacao.java
@@ -1,11 +1,17 @@
 package dev.java10x.itauJava10x;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transacao {
 
-    private BigDecimal valor;
+    private BigDecimal valor; // BigDecimal  - Usado para lidar com precisão
     private OffsetDateTime dataHora; // O OffsetDateTime e Usado para trabalhar com fuso horarios.
 
 }
--- src/main/java/dev/java10x/itauJava10x/TransacoesController.java
+++ src/main/java/dev/java10x/itauJava10x/TransacoesController.java
@@ -1,5 +1,18 @@
 package dev.java10x.itauJava10x;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transacao") // Nome da rota
 public class TransacoesController {
 
+    @PostMapping
+    public ResponseEntity adicionar() {
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }

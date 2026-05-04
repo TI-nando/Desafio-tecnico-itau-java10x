68fcb4a7a73787f6b6dd70a2de4cad9c766080c2
TI-nando <fernandoagro.654@gmail.com>
2026-05-04 17:17:20 -0300

    Primeiro Commit

---
+++ src/main/java/dev/java10x/itauJava10x/Transacao.java
@@ -0,0 +1,11 @@
+package dev.java10x.itauJava10x;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+public class Transacao {
+
+    private BigDecimal valor;
+    private OffsetDateTime dataHora; // O OffsetDateTime e Usado para trabalhar com fuso horarios.
+
+}
---
+++ src/main/java/dev/java10x/itauJava10x/TransacoesController.java
@@ -0,0 +1,5 @@
+package dev.java10x.itauJava10x;
+
+public class TransacoesController {
+
+}

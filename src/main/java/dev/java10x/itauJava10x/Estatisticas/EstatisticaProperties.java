a1e84f0632224af729eb96038084d8eed62751a0
TI-nando <fernandoagro.654@gmail.com>
2026-05-14 10:43:42 -0300

    Fix: Removido o @Component do arquivo Estatisticas / Adicionado @ConfigurationPropertiesScan no itauApplication.java

--- src/main/java/dev/java10x/itauJava10x/Estatisticas/EstatisticaProperties.java
+++ src/main/java/dev/java10x/itauJava10x/Estatisticas/EstatisticaProperties.java
@@ -1,9 +1,7 @@
 package dev.java10x.itauJava10x.Estatisticas;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "estatistica")
 public record EstatisticaProperties (Integer segundos) {
 
--- src/main/java/dev/java10x/itauJava10x/ItauJava10xApplication.java
+++ src/main/java/dev/java10x/itauJava10x/ItauJava10xApplication.java
@@ -2,8 +2,10 @@ package dev.java10x.itauJava10x;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ItauJava10xApplication {
 
 	public static void main(String[] args) {
